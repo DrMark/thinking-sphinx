1377ee8227dabf8b4917fc16e6586c024337196a
DrMark <drmark@gmail.com>
2009-04-25 13:49:31 -1000

    Fixing an i18n bug with TS. Validation messages would fail to translate using the old code.

--- init.rb
+++ init.rb
@@ -7,6 +7,7 @@ end
 if Rails::VERSION::STRING.to_f > 1.2
   require 'action_controller/dispatcher'
   ActionController::Dispatcher.to_prepare :thinking_sphinx do
+    I18n.backend.send(:init_translations) if Rails::VERSION::STRING.to_f > 2.1
     ThinkingSphinx::Configuration.instance.load_models
   end
 end
--- rails/init.rb
+++ rails/init.rb
@@ -1,6 +1,13 @@
 require 'thinking_sphinx'
-require 'action_controller/dispatcher'
 
-ActionController::Dispatcher.to_prepare :thinking_sphinx do
+if Rails::VERSION::STRING.to_f < 2.1
   ThinkingSphinx::Configuration.instance.load_models
+end
+
+if Rails::VERSION::STRING.to_f > 1.2
+  require 'action_controller/dispatcher'
+  ActionController::Dispatcher.to_prepare :thinking_sphinx do
+    I18n.backend.send(:init_translations) if Rails::VERSION::STRING.to_f > 2.1
+    ThinkingSphinx::Configuration.instance.load_models
+  end
 end
